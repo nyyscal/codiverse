f4e3f29b65f45ddd46b51e9cfafc737ac5a3efcd
Nischal <nyyscal541556@gmail.com>
2025-02-27 17:49:38 +0545

    Codiverse WebHooks Added!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
+import Footer from "@/components/providers/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +27,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gradient-to-b from-gray-900 to-gray-950 text-gray-100 flex flex-col`}
+      ><ConvexClientProvider>
         {children}
+      </ConvexClientProvider>
+      <Footer/>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
